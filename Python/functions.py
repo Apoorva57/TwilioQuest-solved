510e7abcdcd8523730c7aef5a93d58abc00c19bf
Apoorva <97695341+Apoorva57@users.noreply.github.com>
2024-11-09 23:29:51 +0530

    Add files via upload

---
+++ Python/functions.py
@@ -0,0 +1,16 @@
+def hail_friend():
+    print("Hail, friend!")
+
+hail_friend()
+
+def hail_friend(x):
+    t = 'Hail ,'
+    print(t, x, '!')
+
+
+hail_friend('Jonathan Joestar')
+
+
+def add_numbers(s, z):
+    y = s + z
+    return y
